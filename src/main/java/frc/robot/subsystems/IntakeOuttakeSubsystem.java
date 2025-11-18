4012964eead6ee20c1ef92c39ff697d3504bddcc
Tim <85653488+Ecume@users.noreply.github.com>
2025-11-18 00:35:16 -0500

    Possible implementation for roborio PID, MotionProfile, FeedForward, and absolute encoder
    The 0.0 parameters are placeholders. This is not final just experimenting.

--- src/main/java/frc/robot/subsystems/IntakeOuttakeSubsystem.java
+++ src/main/java/frc/robot/subsystems/IntakeOuttakeSubsystem.java
@@ -10,6 +10,11 @@ import com.revrobotics.spark.config.ClosedLoopConfig;
 import com.revrobotics.spark.config.SparkMaxConfig;
 import com.revrobotics.spark.config.SparkBaseConfig.IdleMode;
 
+import edu.wpi.first.math.controller.ArmFeedforward;
+import edu.wpi.first.math.controller.PIDController;
+import edu.wpi.first.math.controller.ProfiledPIDController;
+import edu.wpi.first.math.trajectory.TrapezoidProfile;
+import edu.wpi.first.wpilibj.DutyCycleEncoder;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Constants.CANAssignments;
 import frc.robot.util.RobotCANUtils.CANSparkMaxController;
@@ -21,12 +26,24 @@ public class IntakeOuttakeSubsystem extends SubsystemBase {
   private SparkMaxConfig pivotCfg = new SparkMaxConfig();
   private SparkClosedLoopController motorPid;
   private double currentPos;
+  //private ProfiledPIDController pid;
+  //private DutyCycleEncoder absEncoder;
+  //private ArmFeedforward armFeedforward;
+  //private double desiredPos = 0.0;
+  //private static final double ZERO_ROT
+
+  
+
 
 
 
   public IntakeOuttakeSubsystem() {//Parameters are subject to change!
+    //pid = new ProfiledPIDController(0.0, 0.0, 0.0, new TrapezoidProfile.Constraints(0.0,0.0));
+    //armFeedforward = new ArmFeedforward(0.0, 0.0, 0.0);
+    //absEncoder = new DutyCycleEncoder(0);
     pivotMotor = new CANSparkMaxController(CANAssignments.ALGAE_MOTOR_ID, MotorKind.NEO30AMP, pivotCfg, IdleMode.kBrake, 0.1, 0, 0, 0, 0, .6, false);
     motorPid = pivotMotor.getClosedLoopController();
+    //desiredPos = getOffsetedCurrentPos();
   
   }
 
@@ -34,12 +51,16 @@ public class IntakeOuttakeSubsystem extends SubsystemBase {
     
     currentPos = degreesToMotorRot(90); // CHANGE DEGREE TO BE MORE ACCURATE
     //ADD STAR WHEELS ROTATOIN
+    //desiredPos = degreesToMotorRot(90);
+
   
   }
 
   public void intake(){
 
     currentPos = degreesToMotorRot(0);
+    //desiredPos = degreesToMotorRot(90);
+
 
   }
 
@@ -48,10 +69,23 @@ public class IntakeOuttakeSubsystem extends SubsystemBase {
     return (degree/360);
 
   }
-  
+
+//   public double getOffsetedCurrentPos() {
+//     double rot = absEncoder.get() - ZERO_ROT;
+
+//     // wrap into [0,1)
+//     if (rot < 0) 
+//       rot = 0;
+//     return rot;
+// }
 
   @Override
   public void periodic() { // method called once per scheduler run
+    //currentPos = getOffsetedCurrentPos();
+    //must convert pos and vel to radians
+    //double angleRad = pid.getSetpoint.position * 2.0 * Math.PI;
+    //double velRad   = pid.getSetpoint.velocity * 2.0 * Math.PI;
+    //pivotMotor.setVoltage(pid.calculate(currentPos, desiredPos) + armFeedforward.calculate(angleRad, velRad));
     motorPid.setReference(currentPos, ControlType.kMAXMotionPositionControl);
   }
 }
