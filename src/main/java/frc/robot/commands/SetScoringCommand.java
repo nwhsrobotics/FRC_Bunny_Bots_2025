ee499188a64225d95aa74a16fcfc897d55773fbb
NewAmazingPVP <87737759+NewAmazingPVP@users.noreply.github.com>
2025-12-04 18:49:12 -0500

    fix minor bug in robot container and cleaning up set scoring command

--- src/main/java/frc/robot/commands/SetScoringCommand.java
+++ src/main/java/frc/robot/commands/SetScoringCommand.java
@@ -10,11 +10,11 @@ import frc.robot.subsystems.ScoringSubsystem.ScoringStates;
 
 public class SetScoringCommand extends Command {
 
-  ScoringSubsystem subsystem;
-  Timer timer;
-  ScoringStates state;
-  ScoringStates endState;
-  double duration;
+  private final ScoringSubsystem subsystem;
+  private final Timer timer;
+  private final ScoringStates state;
+  private final ScoringStates endState;
+  private final double duration;
 
 
   public SetScoringCommand(ScoringSubsystem subsystem, ScoringStates state, ScoringStates endState, double duration) {
@@ -24,30 +24,25 @@ public class SetScoringCommand extends Command {
     this.duration = duration;
     timer = new Timer();
     addRequirements(subsystem);
-
   }
 
   public SetScoringCommand(ScoringSubsystem subsystem, ScoringStates state, double duration) {
-    this.subsystem = subsystem;
-    this.state = state;
-    this.duration = duration;
-    timer = new Timer();
-    addRequirements(subsystem);
-
+    this(subsystem, state, ScoringStates.Idle, duration);
   }
 
   // Called when the command is initially scheduled.
   @Override
   public void initialize() {
     timer.reset();
-    subsystem.state = state;
     timer.start();
+    subsystem.setState(state);
   }
 
   // Called once the command ends or is interrupted.
   @Override
   public void end(boolean interrupted) {
-    subsystem.state = endState;
+    timer.stop();
+    subsystem.setState(endState);
   }
 
   // Returns true when the command should end.
